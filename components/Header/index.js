e796bf8ed015f9455655bacb3b04bc85b71229a0
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-14 11:17:20 -0500

    Header/index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    // define variables
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+    // setup structure
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(title);
+    headerDiv.appendChild(temp);
+    // add classes
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+    // text
+    date.textContent = 'SMARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return headerDiv;
+}
+
+const headerCont = document.querySelector('.header-container');
+headerCont.appendChild(Header())
